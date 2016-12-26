dce3d11e6d7068654a929f069bd556054e097bfb
Vitor Barros <vitormonteirodebarros@gmail.com>
2016-12-26 17:19:47 -0200

    Ajuestes no modulo

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ var OnBootstrap = function (configFilePath) {
     });
 
     var configFile = require(configFilePath);
+    var preparedObject = {};
 
 };
 
@@ -28,15 +29,53 @@ OnBootstrap.prototype.getConfig = function () {
     return configFile;
 };
 
-OnBootstrap.prototype.prepare = function (url, method, data) {
+/**
+ *
+ * @param service
+ * @param method
+ * @param data
+ * @returns {OnBootstrap}
+ */
+OnBootstrap.prototype.prepare = function (service, method, data) {
 
     method = method.toLowerCase();
     if (method !== 'get' && method !== 'post') {
         throw "This API supports only POST and GET as a HTTP methods"
     }
 
-    
+    preparedObject = {
+        service: service,
+        method: method,
+        data: data
+    };
 
+    return this;
 };
+
+/**
+ * @desc Método que faz a requisição para a CIELO
+ */
+OnBootstrap.prototype.request = function () {
+
+    if (!preparedObject.service || !preparedObject.method) {
+        throw "Please call 'prepared' method before request";
+    }
+
+    //verificando se o serviço está disponível
+    fs.access('services/' + preparedObject.service + ".js", function (err) {
+        if (err) {
+            throw "Service '" + preparedObject.service + "' dos not exists"
+        }
+    });
+
+    var module = require('./services/' + preparedObject.service);
+
+    if(!module.hasOwnProperty('call')){
+        throw "Method 'call' must be implemented"
+    }
+
+    return module.call(preparedObject);
+};
+
 
 module.exports = OnBootstrap;
---
+++ services/sales.js
@@ -0,0 +1,12 @@
+/**
+ * @desc Método que faz uma cobraça simpes com cartão de crédito
+ * @author Vitor Barros
+ * @constructor
+ */
+var Sales = function (preparedObject) {
+    var preparedObject = preparedObject;
+};
+
+Sales.prototype.call = function () {
+
+};
